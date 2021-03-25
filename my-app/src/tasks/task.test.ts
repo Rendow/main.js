0761a306b0dd1344994dc8a721cc64db3185af2b
Rendow <rendow33@gmail.com>
2021-03-25 23:42:12 +0300

    commit

--- my-app/src/tasks/task.test.ts
+++ my-app/src/tasks/task.test.ts
@@ -1,4 +1,5 @@
-import {addFriends, calcSwitch, copyStudents, deepCopyStudents, restParam, StudentType, sum} from "./task";
+import {addFriends, calcSwitch, copyStudents, deepCopyStudents, restParam, StudentType} from "./task";
+import {getBanknoteList, getSum, getTriangleType, isEvenSumGreater, isSquareGreater, sum} from "./tasksStdnt";
 
 
 test('sum of two numbers', () => {
@@ -101,7 +102,45 @@ test('test addFriends', () => {
     expect(copy[1].friends ).toEqual(["Bob", "Nick"])
     expect(copy[2].friends ).toEqual(["Bob","Alex"])
 
+})
+test('test sum', () => {
 
+    expect(sum(2,4,5,1)).toBe(12)
 
+})
+test("get Triangle Type", ()=> {
+    expect(getTriangleType(1, 1, 1)).toBe("10")
+    expect(getTriangleType(3, 3, 2)).toBe("01")
+    expect(getTriangleType(5, 4, 3)).toBe("11")
+    expect(getTriangleType(10, 5, 5)).toBe("00")
+})
+
+test("get Sum ", ()=> {
+    expect(getSum(1000)).toBe(1)
+    expect(getSum(0)).toBe(0)
+    expect(getSum(1234)).toBe(10)
+    expect(getSum(9999)).toBe(36)
+})
+
+test("is Even Sum Greater", ()=> {
+    expect(isEvenSumGreater([1, 100, 2, 200])).toBe(false)
+    expect(isEvenSumGreater([100, 1, 200, 2])).toBe(true)
+})
+
+test("is Square Greater Than Circle", ()=> {
+    const sCr = 3.14
+    const sSq = 4
+    const result = isSquareGreater(sCr, sSq)
+    expect(result).toBe(true)
+})
+
+test("get banknote list", ()=> {
+    const result1500 = getBanknoteList(1500)
+    const result23 = getBanknoteList(23)
+    expect(result1500[0]).toBe(1000)
+    expect(result1500[1]).toBe(500)
+    expect(result23[0]).toBe(0)
+    expect(result23[1]).toBe(1)
+    expect(result23[1]).toBe(2)
 
 })
---
+++ my-app/src/tasks/tasksStdnt.ts
@@ -0,0 +1,143 @@
+// 1. Функция принимает параметром целые положительные числа и возвращает
+// их сумму.
+
+export const sum = (...a: number[]) => {
+    let res = a.reduce((a, b) => a + b)
+    return res
+}
+//... оператор рест, оставшиеся параметры упаковываются в массив(в данном случае все параметры)
+
+// 2. Функция getTriangleType принимает три параметра:
+// длины сторон треугольника.
+// Функция должна возвращать:
+//  - "01", если треугольник равнобедренный,
+//  - "10", если треугольник равносторонний,
+//  - "11", если треугольник обычный,
+//  - "00", если такого треугольника не существует.
+
+export function getTriangleType(a: number, b: number, c: number) {
+    if (a + b > c && a + c > b && c + b > a) {
+        if (a === b && b === c) {
+            return '10'
+        } else if (a === b || c === b || c === a) {
+            return '01'
+        } else {
+            return '11'
+        }
+    } else {
+        return '00'
+    }
+}
+
+
+//3 сумма цифр числа
+
+/*export function getSum(num:number) {
+    let res = num.toString().split('')
+   return  res.reduce((a,b) => a + Number(b),0)
+}*/
+
+//analog
+
+/*
+const getSum = (num:number) => num.toString().split('').reduce((acc, number) =>  acc + +number, 0);
+*/
+
+//analog
+
+/*export function getSum(a: number) {
+    let temp = a.toString();
+    let sum = 0;
+
+    for(let i = 0; i < temp.length; i++) {
+        const res = temp.charAt(i);
+        sum += +res;
+    }
+    return sum
+}*/
+// +res = (+)превращает строку в число
+// charAt(i) - и-тый элемент массива
+
+//analog
+
+
+export function getSum(number: number) {
+
+    let sum = 0;
+    for (; number > 0; number = Math.trunc(number / 10)) {
+        sum += number % 10;
+    }
+    return sum
+}
+
+// 4. Функция принимает isEvenIndexSumGreater параметром массив чисел.
+// Если сумма чисел с чётным индексом (0 как чётный индекс) больше суммы чисел с нечётными индексами,
+// то функция возвращает true. В противном случае - false.
+
+
+/*
+export function isEvenSumGreater(array: number[]) {
+    let odd = 0
+    let even = 0
+    for (let i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            even += array[i]
+        } else {
+            odd += array[i]
+        }
+    }
+    return even > odd
+}*/
+
+/*
+export const isEvenSumGreater = (arr: Array<number>) => {
+    let even = 0
+    let odd = 0
+    arr.reduce((acc, el, index) => index % 2 === 0 ? even += el : odd += el)
+    return even > odd
+}
+*/
+/*
+
+export const isEvenSumGreater = (arr: Array<number>) => {
+    let even = []
+    let odd = []
+  let sum = arr.map((e, i) => {
+      if(i % 2 === 0 ){
+          even.push(e)
+      } else{
+          odd.push(e)
+      }
+  })
+    return even > odd
+}
+*/
+
+export const isEvenSumGreater = (arr: Array<number>) => {
+
+    const sum: number[] = arr.reduce((acc, el, i) => {
+        acc[i % 2] += el
+        return acc
+    }, [0, 0])
+    return sum[0] > sum[1]
+}
+
+// [0, 0] - массив, в одно число четные,  в другое - нечетные
+
+// 5. Функция isSquareGreater принимает два параметра: площадь круга и
+// площадь квадрата. Функция должна возвращать true если круг поместится в
+// квадрате и false в противном случае.
+
+
+export const isSquareGreater = (sCr: number, sSq: number) => {
+    return [1]
+}
+
+// 6. Функция-банкомат принимает параметром целое натуральное число (сумму).
+// Возвращает массив с наименьшим количеством купюр, которыми можно выдать эту
+// сумму. Доступны банкноты следующих номиналов:
+// const banknotes = [1000, 500, 100, 50, 20, 10, 5, 2, 1]
+
+export const getBanknoteList = (total: number) => {
+    return [1]
+}
